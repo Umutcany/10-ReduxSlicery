411d5a4aa1f8810b4bebf16d73cb81a33705ea7d
Umut Can Yilmaz <ylmazumutcan49@gmail.com>
2023-10-03 20:43:00 +0300

    ProjectFinished

--- SliceryProject/src/App.tsx
+++ SliceryProject/src/App.tsx
@@ -12,7 +12,7 @@ function App() {
       <CourseForm />
       <CourseSearch />
       <CourseList />
-      {/* <CourseValue /> */}
+      <CourseValue />
     </div>
   );
 }
--- SliceryProject/src/components/CourseValue.tsx
+++ SliceryProject/src/components/CourseValue.tsx
@@ -1,7 +1,28 @@
 import React from "react";
+import { useSelector } from "react-redux";
+
+interface RootStateType {
+  courses: {
+    data: {
+      name: string;
+      cost: number;
+    }[];
+    searchTerm: string;
+  };
+}
 
 const CourseValue = () => {
-  return <div>CourseValue</div>;
+  const { data, searchTerm } = useSelector(
+    (state: RootStateType) => state.courses
+  );
+
+  const totalCost = data
+    .filter((course) =>
+      course.name.toLowerCase().includes(searchTerm.toLowerCase())
+    )
+    .reduce((acc, course) => acc + course.cost, 0);
+
+  return <div className="coursePrice">Toplam Tutar: {totalCost}</div>;
 };
 
 export default CourseValue;
